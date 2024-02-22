d2b83940d3e2b8911e3192ba58eb4e8b246e6b40
Uditya1999 <udityanarayanmishra@gmail.com>
2024-02-23 00:29:48 +0530

    fully added cart layout

--- app/src/main/java/com/example/wedokart/activity/CartActivity.java
+++ app/src/main/java/com/example/wedokart/activity/CartActivity.java
@@ -1,20 +1,44 @@
 package com.example.wedokart.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
 import com.example.wedokart.R;
+import com.example.wedokart.adapters.CartAdapter;
 import com.example.wedokart.databinding.ActivityCartBinding;
+import com.example.wedokart.model.Product;
+
+import java.util.ArrayList;
 
 public class CartActivity extends AppCompatActivity {
 
     ActivityCartBinding binding;
+    CartAdapter adapter;
+    ArrayList<Product> products;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding= ActivityCartBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        products = new ArrayList<>();
+
+        products.add(new Product("Product1", "https://thebrandhopper.com/wp-content/uploads/2021/10/Product-Innovation.jpg", "321", 50.0, 50.0, 12, 1));
+        products.add(new Product("Product1", "https://thebrandhopper.com/wp-content/uploads/2021/10/Product-Innovation.jpg", "321", 50.0, 50.0, 12, 1));
+        products.add(new Product("Product1", "https://thebrandhopper.com/wp-content/uploads/2021/10/Product-Innovation.jpg", "321", 50.0, 5.0, 12, 1));
+
+
+        adapter = new CartAdapter(this, products);
+
+        LinearLayoutManager layoutManager= new LinearLayoutManager(this);
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(this, layoutManager.getOrientation());
+        binding.cartList.setLayoutManager(layoutManager);
+        binding.cartList.addItemDecoration(itemDecoration);
+        binding.cartList.setAdapter(adapter);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
---
+++ app/src/main/java/com/example/wedokart/adapters/CartAdapter.java
@@ -0,0 +1,59 @@
+package com.example.wedokart.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.wedokart.R;
+import com.example.wedokart.databinding.ItemCartBinding;
+import com.example.wedokart.model.Product;
+
+import java.util.ArrayList;
+
+public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder> {
+
+    Context context;
+    ArrayList<Product> products ;
+
+    public CartAdapter(Context context, ArrayList<Product> products) {
+        this.context = context;
+        this.products = products;
+    }
+
+    @NonNull
+    @Override
+    public CartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new CartViewHolder(LayoutInflater.from(context).inflate(R.layout.item_cart, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CartViewHolder holder, int position) {
+        Product product = products.get(position);
+        Glide.with(context)
+                .load(product.getImage())
+                .into(holder.binding.image);
+
+        holder.binding.name.setText(product.getName());
+        holder.binding.price.setText("PKR " + product.getPrice());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+
+    public class CartViewHolder extends RecyclerView.ViewHolder {
+
+        ItemCartBinding binding;
+        public CartViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = ItemCartBinding.bind(itemView);
+        }
+    }
+}
